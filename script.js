cbd02d2e0dd14ee9a485734b7e2761c8dcc90748
bradg4508 <64989476+bradg4508@users.noreply.github.com>
2023-03-24 12:51:23 -0500

    Clarify comments to precisely address variables

--- script.js
+++ script.js
@@ -38,10 +38,10 @@ const equals = document.querySelector("#equals button");
 const clearAll = document.querySelector("#clear button");
 
 function setValue(e) {
-    // If the user presses the "Equals" button consecutively more than once,
+    // If the user presses the Equals button consecutively more than once,
     //      the numArray will only include the number once
     // It allows the user to continue performing operations with accurate results
-    // The variable "count" is arbitrary and only needed to indicate that an operation has occurred
+    // The variable count is arbitrary and only needed to indicate that an operation has occurred
     if (count === 1) {
         numArray.splice(0, 1);
     }
@@ -51,7 +51,7 @@ function setValue(e) {
     // Display the value in a div above the calculator buttons
     num = +valueString.textContent;
 
-    // Reset the "count" variable to allow for successive operations to be performed
+    // Reset the count variable to allow for successive operations to be performed
     //      after one has already been completed
     count = 0;
 }
@@ -72,14 +72,14 @@ function setOperator(e) {
     // Add the operator to the numArray
     numArray.push(operator);
     
-    // If the user presses the same "operator" button more than once,
+    // If the user presses the same operator button more than once,
     //      remove the repeated occurrences of the operator so that only one remains in the numArray
     if ((numArray[numArray.indexOf(operator)] === numArray[numArray.indexOf(operator) + 1])) {
         numArray.splice(numArray.indexOf(operator) + 1, 1);
     }
-    // If the user continuously presses different "operator" buttons more than once,
-    //      the last "operator" button that is pressed will be the one involved in the ongoing operation
-    // Remove any other previous button presses of other "operator" buttons from the numArray 
+    // If the user continuously presses different operator buttons more than once,
+    //      the last operator button that is pressed will be the one involved in the ongoing operation
+    // Remove any other previous button presses of other operator buttons from the numArray 
     if((typeof numArray[numArray.indexOf(operator) - 1] === "string") ||
        (numArray.indexOf(operator) === 0)) {
         numArray.splice(numArray.indexOf(operator) - 1, 1);
@@ -100,31 +100,30 @@ function performOperation() {
 function displayCalculation() {
     // Before the calculation is performed, add the the user's most recently entered value 
     //      to the numArray
-    // If the user presses the "Enter" button without entering any values yet,
+    // If the user presses the Equals button without entering any values yet,
     //      the numArray will not add an empty string as an element
     if (valueString.textContent !== "") {
         numArray.push(num);
     }
-    // If the user presses the "Enter" button more than once without adding an operation,
+    // If the user presses the Equals button more than once without adding an operation,
     //      the numArray will only include the previously entered value as its initial element
     if (numArray.length === 2 && (numArray[0] === numArray[1])) {
         numArray.splice(0, 1);
     }
     // Check to see if the numArray has at least three elements
     if (numArray.length >= 3) {
-        // If the user presses the "Enter" button consecutively while entering an operation,
+        // If the user presses the Equals button consecutively while entering an operation,
         //      the numArray will replace any repeated values so that only one exists
         if ((numArray[0] === numArray[1]) || 
             (numArray[1] === numArray[2])) {
             numArray.splice(0, 2);
         } else {
             // Display the current answer to an operation
-            // Change the "count" variable to allow the user to repeatedly press the "Enter" button without
+            // Change the count variable to allow the user to repeatedly press the Equals button without
             //      it affecting the elements in the numArray
             num = performOperation();
             valueString.textContent = num;
             count = 1;
-            console.log(numArray);
         }
     }
 }
@@ -132,7 +131,7 @@ function clearValues() {
     // Empty the numArray so that no values or operators currently exist
     numArray.length = 0;
 
-    // Reset the "num" variable after clearing the calculator so that future operations are not impacted by it
+    // Reset the num variable after clearing the calculator so that future operations are not impacted by it
     num = "";
 
     // Show an empty string in the display area
@@ -152,10 +151,10 @@ digits.forEach((digit) => {
         operator.addEventListener("click", setOperator);
     });
 
-    // Add an event listener to the "Enter" button (=) to perform the current operation
+    // Add an event listener to the Equals button (=) to perform the current operation
     equals.addEventListener("click", displayCalculation);
 
-    // Add an event listener to the "All Clear" button (AC) to reset the calculator and end any ongoing operations
+    // Add an event listener to the All Clear button (AC) to reset the calculator and end any ongoing operations
     clearAll.addEventListener("click", clearValues);
     
 });
@@ -164,5 +163,3 @@ digits.forEach((digit) => {
 // ISSUES STILL TO RESOLVE
 //  -Can still enter values that add on to the most recent total that has just been calculated
 //      -I want the new value to override the current total in the display area
-//  -Set only the last clicked "operator" button to apply to the next value
-//      -Right now, each press adds the previous "num" to be added to the numArray
